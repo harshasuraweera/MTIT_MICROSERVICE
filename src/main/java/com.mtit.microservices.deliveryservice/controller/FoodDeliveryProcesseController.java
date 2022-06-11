0dbca6e15293b8ca28895d4d6377f250bd39267d
NadunBandara <68751694+NadunBandara@users.noreply.github.com>
2022-06-11 20:21:50 +0530

    Controller classes

--- src/main/java/com.mtit.microservices.deliveryservice/controller/FoodDeliveryProcesseController.java
+++ src/main/java/com.mtit.microservices.deliveryservice/controller/FoodDeliveryProcesseController.java
@@ -1,4 +1,25 @@
 package com.mtit.microservices.deliveryservice.controller;
 
+import com.mtit.microservices.deliveryservice.dtos.DeliveryRequest;
+import com.mtit.microservices.deliveryservice.dtos.DeliveryResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/processeDelivery")
 public class FoodDeliveryProcesseController {
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public @ResponseBody
+    DeliveryResponse item(@RequestBody DeliveryRequest DeliveryRequest){
+
+        System.out.println("Item Details: "+ DeliveryRequest);
+
+        var deliveryresponse = new DeliveryResponse();
+        deliveryresponse.setItemId(UUID.randomUUID().toString());
+        deliveryresponse.setMessage("Successfully found the Processe Delivery..");
+
+        return deliveryresponse;
+
+    }
 }
--- src/main/java/com.mtit.microservices.deliveryservice/controller/FoodToBeDeliveryController.java
+++ src/main/java/com.mtit.microservices.deliveryservice/controller/FoodToBeDeliveryController.java
@@ -1,4 +1,25 @@
 package com.mtit.microservices.deliveryservice.controller;
 
+import com.mtit.microservices.deliveryservice.dtos.DeliveryRequest;
+import com.mtit.microservices.deliveryservice.dtos.DeliveryResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/toBeDelivery")
 public class FoodToBeDeliveryController {
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public @ResponseBody
+    DeliveryResponse item(@RequestBody DeliveryRequest DeliveryRequest){
+
+        System.out.println("Item Details: "+ DeliveryRequest);
+
+        var deliveryresponse = new DeliveryResponse();
+        deliveryresponse.setItemId(UUID.randomUUID().toString());
+        deliveryresponse.setMessage("Successfully found to be Delivery..");
+
+        return deliveryresponse;
+
+    }
 }
