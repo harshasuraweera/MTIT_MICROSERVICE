c218011ea091cedf03303f6863404d99365d1f4b
NadunBandara <68751694+NadunBandara@users.noreply.github.com>
2022-06-11 20:17:50 +0530

    classes

--- src/main/java/com.mtit.microservices.deliveryservice/dtos/ToBeDeliveryResponse.java
+++ src/main/java/com.mtit.microservices.deliveryservice/dtos/ToBeDeliveryResponse.java
@@ -1,4 +1,31 @@
 package com.mtit.microservices.deliveryservice.dtos;
 
 public class ToBeDeliveryResponse {
+
+    private String foodId;
+    private String message;
+
+    public String getItemId() {
+        return foodId;
+    }
+
+    public void setItemId(String itemId) {
+        this.foodId = foodId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "DeliveryResponse{" +
+                "foodId='" + foodId + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
