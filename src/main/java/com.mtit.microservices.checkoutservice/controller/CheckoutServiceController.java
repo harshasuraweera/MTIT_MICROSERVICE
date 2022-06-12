f2f43fbb7583d9f533edc9445e6a264e1d8eef93
Harsha Suraweera <harshamanoj912@gmail.com>
2022-06-12 14:25:33 +0530

    Delete from cart is completed

--- src/main/java/com.mtit.microservices.checkoutservice/controller/CheckoutServiceController.java
+++ src/main/java/com.mtit.microservices.checkoutservice/controller/CheckoutServiceController.java
@@ -8,7 +8,6 @@ import com.mtit.microservices.checkoutservice.utils.Cart;
 import com.mtit.microservices.checkoutservice.utils.CartRepository;
 import com.mtit.microservices.checkoutservice.utils.Food;
 import com.mtit.microservices.checkoutservice.utils.FoodRepository;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -59,23 +58,26 @@ public class CheckoutServiceController {
         return new ResponseEntity<List<Food>>(foodRepository.findByFoodName(keyword), HttpStatus.OK);
     }
 
-//    @DeleteMapping(path = "/deleteFromCart", consumes = "application/json", produces = "application/json")
-//    public ResponseEntity<Integer> deleteFromCart(@RequestBody String jsonString){
-//        JSONObject jsonObject = new JSONObject(jsonString);
-//        return new ResponseEntity<Integer>(cartRepository.deleteFromCart(jsonObject.getString("foodId"), jsonObject.getString("customerId")), HttpStatus.OK);
-//    }
 
     @DeleteMapping(path = "/deleteFromCart", consumes = "application/json", produces = "application/json")
-    public @ResponseBody ResponseEntity<Integer> item(@RequestBody DeleteFromCartRequest deleteFromCartRequest) {
-
+    public @ResponseBody DeleteFromCartResponse item(@RequestBody DeleteFromCartRequest deleteFromCartRequest) {
         System.out.println("Postman Results: " + deleteFromCartRequest);
         var deleteFromCartResponse = new DeleteFromCartResponse();
 
 
         ResponseEntity<Integer> result = new ResponseEntity<Integer>(cartRepository.deleteFromCart(deleteFromCartRequest.getFoodId(), deleteFromCartRequest.getCustomerId()), HttpStatus.OK);
 
+        System.out.println(result);
 
-        return result;
+        if((result.toString().equals("<200 OK OK,1,[]>"))){
+            deleteFromCartResponse.setFoodId(deleteFromCartRequest.getFoodId());
+            deleteFromCartResponse.setStatusMessage("The item is successfully delete from the cart.");
+        }else{
+            deleteFromCartResponse.setFoodId(deleteFromCartRequest.getFoodId());
+            deleteFromCartResponse.setStatusMessage("The item cannot delete from the cart or maybe it is not existing in the cart. Please refresh the page.");
+        }
+
+        return deleteFromCartResponse;
 
     }
 
